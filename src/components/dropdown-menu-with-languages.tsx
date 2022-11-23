67db328a561ab1371a741c0e6489c0bb36e77d34
Artur <helter@wp.pl>
2022-11-23 18:14:22 +0100

    add more style to DropdownMenuWithLanguages

--- src/components/dropdown-menu-with-languages.tsx
+++ src/components/dropdown-menu-with-languages.tsx
@@ -4,19 +4,36 @@ import Germany from '../assets/img/flags/germany-flag-icon.svg'
 import Italy from '../assets/img/flags/italy.svg'
 
 const DropContainer = styled.div`
+    display: none;
     position: absolute;
     left: 0;
     top: 3rem;
-    width: 8rem;
+    width: 10rem;
     border: 1px solid #A4BE7B;
     border-radius: 6px;
+`
+
+const RowLanguageContainer = styled.div`
+    width:100%;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    cursor: pointer;
+    & p{
+        opacity: 0.5;
+    }
+
+    &:hover{
+        background-color: #A4BE7B;
+        & p{
+            opacity: 1;
+        }    
+    }
 `
 const SingleImageContainer = styled.div`
     padding: 0 0.5rem;
     width: 2rem;
-    display: flex;
-    gap: 0.5rem;
-    align-items: center
+  
     & img{
         width: 100%;
         height: 100%;
@@ -29,18 +46,25 @@ const SingleImageContainer = styled.div`
 const DropdownMenuWithLanguages = () => {
   return (
     <DropContainer>
-      <SingleImageContainer>
-        <img src={American} alt='flag'/>
-        <p>English</p>
-      </SingleImageContainer>
-      <SingleImageContainer>
-        <img src={Germany} alt='flag'/>
+        <RowLanguageContainer>
+            <SingleImageContainer>
+                <img src={American} alt='flag'/>
+            </SingleImageContainer>
+            <p>English</p>
+        </RowLanguageContainer>
+      <RowLanguageContainer>
+        <SingleImageContainer>
+            <img src={Germany} alt='flag'/>
+        </SingleImageContainer>
         <p>German</p>
-      </SingleImageContainer>
-      <SingleImageContainer>
-        <img src={Italy} alt='flag'/>
+      </RowLanguageContainer>
+      <RowLanguageContainer>
+        <SingleImageContainer>
+            <img src={Italy} alt='flag'/>
+        </SingleImageContainer>
         <p>Italian</p>
-      </SingleImageContainer>
+      </RowLanguageContainer>
+      
     </DropContainer>
   )
 }
