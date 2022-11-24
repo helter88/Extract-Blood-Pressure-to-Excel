6eb69680bddf7d442ee08f42e1841221125b96d7
Artur <helter@wp.pl>
2022-11-24 17:25:28 +0100

    extract code from DropdownMenuWithLanguages to LanguageDropdownRow

--- src/components/dropdown-menu-with-languages.tsx
+++ src/components/dropdown-menu-with-languages.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import American from '../assets/img/flags/american.svg'
-import Germany from '../assets/img/flags/germany-flag-icon.svg'
-import Italy from '../assets/img/flags/italy.svg'
+
 import useLanguages from '../hooks/use-languages';
+import LanguageDropdownRow from './language-dropdown-row';
 
 const DropContainer = styled.div<OpenStatus>`
     /* display: ${({openStatus}) => !openStatus && "none"}; */
@@ -14,33 +13,8 @@ const DropContainer = styled.div<OpenStatus>`
     border-radius: 6px;
 `
 
-const RowLanguageContainer = styled.div`
-    width:100%;
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-    cursor: pointer;
-    & p{
-        opacity: 0.5;
-    }
 
-    &:hover{
-        background-color: #A4BE7B;
-        & p{
-            opacity: 1;
-        }    
-    }
-`
-const SingleImageContainer = styled.div`
-    padding: 0 0.5rem;
-    width: 2rem;
-  
-    & img{
-        width: 100%;
-        height: 100%;
-    }
-   
-`
+
 interface OpenStatus {
     openStatus: boolean
 }
@@ -52,24 +26,9 @@ const DropdownMenuWithLanguages = ({openStatus}: OpenStatus) => {
     console.log("list of languages is:", languages)
   return (
     <DropContainer openStatus={openStatus}>
-        <RowLanguageContainer>
-            <SingleImageContainer>
-                <img src={American} alt='flag'/>
-            </SingleImageContainer>
-            <p>English</p>
-        </RowLanguageContainer>
-      <RowLanguageContainer>
-        <SingleImageContainer>
-            <img src={Germany} alt='flag'/>
-        </SingleImageContainer>
-        <p>German</p>
-      </RowLanguageContainer>
-      <RowLanguageContainer>
-        <SingleImageContainer>
-            <img src={Italy} alt='flag'/>
-        </SingleImageContainer>
-        <p>Italian</p>
-      </RowLanguageContainer>
+       <LanguageDropdownRow language="English"/>
+       <LanguageDropdownRow language="German"/>
+       <LanguageDropdownRow language="Italian"/>
       
     </DropContainer>
   )
---
+++ src/components/language-dropdown-row.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import English from '../assets/img/flags/american.svg'
+import Germany from '../assets/img/flags/germany-flag-icon.svg'
+import Italian from '../assets/img/flags/italy.svg'
+import Polish from '../assets/img/flags/poland.svg'
+
+const RowLanguageContainer = styled.div`
+    width:100%;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    cursor: pointer;
+    & p{
+        opacity: 0.5;
+    }
+
+    &:hover{
+        background-color: #A4BE7B;
+        & p{
+            opacity: 1;
+        }    
+    }
+`
+const SingleImageContainer = styled.div`
+    padding: 0 0.5rem;
+    width: 2rem;
+  
+    & img{
+        width: 100%;
+        height: 100%;
+    }
+   
+`
+
+const LanguageDropdownRow = ({language}:{language:string}) => {
+  return (
+    <RowLanguageContainer>
+        <SingleImageContainer>
+            <img src={English} alt='flag'/>
+        </SingleImageContainer>
+         <p>{language}</p>
+    </RowLanguageContainer>
+  )
+}
+
+export default LanguageDropdownRow 
