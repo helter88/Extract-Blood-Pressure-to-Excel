9510cd071081abb634bed998d5441735fa7a9ac3
Artur <helter@wp.pl>
2022-11-29 10:45:08 +0100

    add style to textarea in InputBox

--- src/components/input-box.tsx
+++ src/components/input-box.tsx
@@ -1,9 +1,23 @@
+import styled from "styled-components"
+
+const InputBoxContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  & textarea{
+    outline: none;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    background-color:#107C41;
+    color : white;
+  }
+`
 
 const InputBox = () => {
   return (
-    <div>
-      InputBox
-    </div>
+    <InputBoxContainer >
+      <label htmlFor="results">Add your results</label>
+      <textarea id="results" name="results" rows={25} cols={30}/>
+    </InputBoxContainer >
   )
 }
 
