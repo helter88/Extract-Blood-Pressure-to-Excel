5bbf6f24823cb01fd257a1b8d1770ffd1bfb0a8d
Artur <helter@wp.pl>
2023-01-13 13:00:14 +0100

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/header";
-import { MainCointainer } from "./components/main-container";
-import AddToFile from "./pages/add-to-file";
-import CreateFile from "./pages/create-file";
-import FirstPage from "./pages/first-page";
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import { MainCointainer } from './components/main-container';
+import AddToFile from './pages/add-to-file';
+import CreateFile from './pages/create-file';
+import FirstPage from './pages/first-page';
 
 const App = () => {
-  return (
-    <MainCointainer>
-      <Header />
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/create-file" element={<CreateFile />} />
-        <Route path="/add-to-file" element={<AddToFile />} />
-      </Routes>
-    </MainCointainer>
-  );
+	return (
+		<MainCointainer>
+			<Header />
+			<Routes>
+				<Route path="/" element={<FirstPage />} />
+				<Route path="/create-file" element={<CreateFile />} />
+				<Route path="/add-to-file" element={<AddToFile />} />
+			</Routes>
+		</MainCointainer>
+	);
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./app";
-import "./utils/i18next";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import './utils/i18next';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
